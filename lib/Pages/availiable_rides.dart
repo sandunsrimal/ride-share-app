a6db13ce2b0da570e6718f2697fcfc80337aebe9
sandunsrimal <bandara-cs19005@stu.kln.ac.lk>
2023-10-28 11:01:59 +0530

    update

--- lib/Pages/availiable_rides.dart
+++ lib/Pages/availiable_rides.dart
@@ -127,14 +127,8 @@ class _AvailiableRidesState extends State<AvailiableRides> {
     double a = 0.5 -
         cos((lat2 - lat1) * p) / 2 +
         cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2;
-// print(lat1);
-// print(lat2);
-// print(lon1);
-// print(lon2);
-    // print(lat1);
-    // print(lat2);
-    // print(lon1);
-// print(lon2);
+
+
     return 12742 * asin(sqrt(a));
   }
 
